d09af5907c6ecf9d491a20841e362973b786767e
Anaximandr228 <alex09.g29@gmail.com>
2024-08-30 19:32:27 +0500

    edit user model

--- app/models.py
+++ app/models.py
@@ -8,8 +8,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(255), nullable=False)
-    last_name = Column(String(255), nullable=False)
     username = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
 
