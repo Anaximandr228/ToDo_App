90a5aad27cb8865e1eeeda0d2bc5ec5338a60609
Anaximandr228 <alex09.g29@gmail.com>
2024-08-30 19:32:58 +0500

    add registration

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,4 @@
 import json
-
 import requests
 from sqlalchemy.orm import Session
 import models
@@ -15,19 +14,20 @@ def create_user_note(db: Session, note: shemas.NoteCreate, user_id: int):
     db_note = models.Note(**note.dict(), owner_id=user_id)
     url_content = f'https://speller.yandex.net/services/spellservice.json/checkTexts?text={db_note.content}'
     url_title = f'https://speller.yandex.net/services/spellservice.json/checkTexts?text={db_note.title}'
-    # r = requests.get(url_content)
-    # response_json = json.loads(r.content)
-    #
-    # for suggestion in response_json[0]:
-    #     db_note.content = db_note.content.replace(suggestion['word'], suggestion['s'][0])
     db_note.content = edittext(db_note.content, url_content)
     db_note.title = edittext(db_note.title, url_title)
-
-
     db.add(db_note)
     db.commit()
     db.refresh(db_note)
     return db_note
+
+
+def create_user(db: Session, user: shemas.UserCreate):
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
 def edittext(text, url):
