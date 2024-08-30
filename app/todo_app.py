3652845b749d0bd027afb6cd1520a91dc3f5e70d
Anaximandr228 <alex09.g29@gmail.com>
2024-08-30 19:31:54 +0500

    add registration

--- app/todo_app.py
+++ app/todo_app.py
@@ -1,17 +1,17 @@
 from typing import Annotated
-
 import uvicorn
 from fastapi import FastAPI, HTTPException, Depends, status
 from sqlalchemy.orm import Session
-from app import models, crud
+import models
+import crud
 import shemas
-from app.database import engine, SessionLocal
+from database import engine, SessionLocal
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 models.Base.metadata.create_all(bind=engine)
 
 security = HTTPBasic()
-app = FastAPI(dependencies=[Depends(security)])
+app = FastAPI()
 
 
 def get_db():
@@ -22,35 +22,10 @@ def get_db():
         db.close()
 
 
-@app.get("/users/{user_id}", response_model=list[shemas.Note])
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_notes = crud.get_user_notes(db, user_id=user_id)
-    if db_notes is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_notes
-
-
-@app.post("/users/{user_id}/notes/", response_model=shemas.Note)
-def create_note_for_user(
-    user_id: int, note: shemas.NoteCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_note(db=db, note=note, user_id=user_id)
-
-
-users = {
-    "admin": {
-        "password": "Password123",
-        "token": "",
-        "priviliged": True
-    }
-}
-
-
 def get_current_username(
-    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
-):
+        credentials: Annotated[HTTPBasicCredentials, Depends(security)], db: Session = Depends(get_db)):
     current_username_bytes = credentials.username
-    user = users.get(current_username_bytes)
+    user = db.query(models.User).filter(models.User.username == current_username_bytes).first()
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -58,13 +33,33 @@ def get_current_username(
             headers={"WWW-Authenticate": "Basic"},
         )
 
-    if not credentials.password == user['password']:
+    if not credentials.password == user.password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Basic"},
         )
-    return credentials.username
+    return user.id
+
+
+@app.get("/notes", response_model=list[shemas.Note])
+def read_user(user_id: Annotated[int, Depends(get_current_username)], db: Session = Depends(get_db)):
+    db_notes = crud.get_user_notes(db, user_id=user_id)
+    if db_notes is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_notes
+
+
+@app.post("user/register", response_model=shemas.User)
+def create_user(user: shemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
+
+
+@app.post("/note", response_model=shemas.Note)
+def create_note_for_user(user_id: Annotated[int, Depends(get_current_username)],
+                         note: shemas.NoteCreate, db: Session = Depends(get_db)
+                         ):
+    return crud.create_user_note(db=db, note=note, user_id=user_id)
 
 
 @app.get("/user/me")
